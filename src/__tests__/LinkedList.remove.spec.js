ead3d1c09220918ce0d0e21ac878841b8fd7b520
Gabin Desserprit <desserprit.gabin@gmail.com>
2020-09-23 19:50:33 +0200

    More tests

--- src/__tests__/LinkedList.remove.spec.js
+++ src/__tests__/LinkedList.remove.spec.js
@@ -30,4 +30,64 @@ describe('remove(index)', () => {
       },
     ])
   })
+
+  it('should remove in a well referrenced list', () => {
+    const LL = localInstance()
+    LL.init([
+      {
+        id: '2',
+        next: { id: '3' },
+      },
+      {
+        id: '1',
+        next: { id: '2' },
+      },
+      {
+        id: '3',
+      },
+    ])
+
+    LL.remove(0)
+
+    expect(LL.list).toEqual([
+      {
+        id: '2',
+        next: { id: '3' },
+      },
+      {
+        id: '3',
+        next: { id: null },
+      },
+    ])
+  })
+
+  it('should remove in a well referrenced list', () => {
+    const LL = localInstance()
+    LL.init([
+      {
+        id: '2',
+        next: { id: '3' },
+      },
+      {
+        id: '1',
+        next: { id: '2' },
+      },
+      {
+        id: '3',
+      },
+    ])
+
+    LL.remove(2)
+
+    expect(LL.list).toEqual([
+      {
+        id: '1',
+        next: { id: '2' },
+      },
+      {
+        id: '2',
+        next: { id: null },
+      },
+    ])
+  })
 })
